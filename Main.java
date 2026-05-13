61e7fc16616badf483e4a729ab135d5572aff788
rivamukhlas <rivamukhlas@gmail.com>
2026-05-13 10:29:56 +0700

    memperbaiki eror

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Produk p1 = new Elektronik("Samsung", 100000, 5, 2);
         Produk p2 = new Makanan("Bakso", 1000, 100, 3);
-        Kasir kasir = new Kasir("Maman")
+        Kasir kasir = new Kasir("Maman");
     }
 
     p1.tampilInfo();
