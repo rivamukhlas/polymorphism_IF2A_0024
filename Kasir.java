ab24ba4654f14562625c23349f8b8e530e812c66
rivamukhlas <rivamukhlas@gmail.com>
2026-05-13 09:52:38 +0700

    Add Proses Belanja

--- Kasir.java
+++ Kasir.java
@@ -10,3 +10,19 @@ public class Kasir {
         System.out.println("Nama Kasir: " + namaKasir);
     }
 
+    public void prosesBelanja(Produk p, int jmlhBeli) {
+
+        if (p.getStok() < jmlhBeli) {
+            System.out.println("Out of stok");
+        }
+
+        {
+            double hargaAkhir = p.hitungHargaAkhir(jmlhBeli);
+            System.out.println("Harga Akhir = " + hargaAkhir);
+            p.setStok(jmlhBeli);
+        }
+
+        System.out.println("Stok terbaru= " + p.getStok());
+    }
+}
+
