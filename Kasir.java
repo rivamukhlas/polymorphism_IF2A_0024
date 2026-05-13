4eea9d94e24b9d182b48518b1a9c0199a33cad85
rivamukhlas <rivamukhlas@gmail.com>
2026-05-13 09:48:58 +0700

    first commit

---
+++ Kasir.java
@@ -0,0 +1,14 @@
+public class Kasir {
+    private String namaKasir;
+
+    public Kasir(String namaKasir) {
+        this.namaKasir = namaKasir;
+    }
+
+    public void tampilkIndentitas() {
+        System.out.println("===== TOKO GARUDA =====");
+        System.out.println("Nama Kasir: " + namaKasir);
+    }
+ }
+
+
