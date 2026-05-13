7a3da945eea5c6de8d92857fbb582a07bdd923e5
rivamukhlas <rivamukhlas@gmail.com>
2026-05-13 10:03:17 +0700

    Add atribute

--- Elektronik.java
+++ Elektronik.java
@@ -1,3 +1,14 @@
-public class Elektronik {
+public class Elektronik extends Produk {
+
+    private int masaGaransi;
+
+    public Elektronik(String nama, double harga, int stok, int masaGaransi) {
+        super(nama, harga, stok);
+        this.masaGaransi = masaGaransi;
+    }
+
+
+    
+
     
 }
