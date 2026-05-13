773ee1ea4195d7b3287ad5b4d0bf25636ef52e5c
rivamukhlas <rivamukhlas@gmail.com>
2026-05-13 10:20:55 +0700

    Add objek kasir dan produk

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        Kasir kasir = new Kasir();
+        Makanan makanan = new Makanan();
+        Elektronik elektronik = new Elektronik();
+        Produk produk = new Produk();
+    }
+}
    
 
