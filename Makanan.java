973c46222f89ef3149e20ed2d9592da1a34d0247
rivamukhlas <rivamukhlas@gmail.com>
2026-05-13 09:55:52 +0700

    Create Class Makanan

--- Makanan.java
+++ Makanan.java
@@ -1,3 +1,9 @@
-public class Makanan {
+public class Makanan extends Produk {
+    private int kadaluarsa;
+
+    public Makanan(String nama, double harga, int stok, int kadaluarsa) {
+        super(nama, harga, stok);
+        this.kadaluarsa = kadaluarsa;
+    }
     
 }
